452d5a8776b322fea91d0b0d0425a2f4887f962b
adityad171 <97349366+adityad171@users.noreply.github.com>
2022-11-24 22:07:25 +0530

    Add files via upload

---
+++ ques2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <linux/kernel.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define TWONED_COPY_SYSCALL 451
+
+int main()
+{
+    int src_arr[][] = {{1, 2, 3, 4, 5},{6,7,8,9,0}};
+    int dest_arr[2][5]; 
+
+    long sys_call_status;
+
+    sys_call_status = syscall(TWOD_COPY_SYSCALL, src_arr, dest_arr, 5);
+
+    if (sys_call_status != EFAULT)
+    {
+        printf("Message : System Call 451 successfuly invoked \n");
+        {
+        int i;
+        for (i = 0; i < 2; i++)
+        {
+            for (j = 0; j < 5; j++)
+            {
+                if (src_arr[i][j] != dest_arr[i][j])
+                {
+                    printf("Error : LHS[%d][%d] != RHS[%d][%d}\n", i,j,i,j );
+                    return;
+                }
+            }
+        }
+        printf("Message : Success LHS = RHS \n");
+        }
+    
+    }
+
+    return 0;
+}
